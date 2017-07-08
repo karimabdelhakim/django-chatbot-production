d66de175bfc4b355144f06d73fb0482bbce3c10c
karim <karimabdelhakim22@hotmail.com>
2017-07-08 16:53:12 +0200

    add profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+    
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    delHistoryDate = models.DateTimeField(blank=True, null=True, default=None)
+
+
+    def __unicode__(self):
+        return self.user.username
+    
